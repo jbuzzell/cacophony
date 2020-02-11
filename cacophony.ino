d6f4cde122efd3a73f5c23930e258002918e63e8
Jake Buzzell <jacob.buzzell@mymail.champlain.edu>
2020-02-11 15:54:17 -0500

    Updated Arduino sketch for demo

--- cacophony.ino
+++ cacophony.ino
@@ -8,38 +8,44 @@
 // GUItool: begin automatically generated code
 AudioInputI2S             i2s1;           //xy=331,237
 AudioAnalyzeNoteFrequency freq;      //xy=519,240
-AudioConnection           patchCord1(i2s1, 0, freq, 0);
+AudioConnection           patchCord1(i2s1, 1, freq, 0);
 AudioControlSGTL5000      audioControl;     //xy=633,517
 // GUItool: end automatically generated code
 // TODO: create dynamic storage for input notes
 
 void setup() {
-  Serial.begin(9600);
-
   audioControl.enable();
   audioControl.inputSelect(AUDIO_INPUT_LINEIN);
-
+  Serial.begin(9600);
   AudioMemory(30);
-  
+
   freq.begin(.15);
 }
 
 void loop() {
   // TODO: MAIN LOOP
   // IF SWITCH IS ACTIVE, LISTEN FOR INPUT
-  
-  if(freq.available() && freq.probability() > 0.75){
+  if (freq.available() && freq.probability() > 0.75) {
     createMIDINote();
+  } else {
+    Serial.println("no note detected");
   }
+
+  delay(250);
 }
 
-void createMIDINote(){
+void createMIDINote() {
   double  tmp = double(freq.read()),
           noteFreq = tmp;
+  Note note = Note(noteFreq);
 
   //TODO: PREVENT DUPLICATE NOTES FROM BEING PUSHED TO VECTOR
 
-  if(noteFreq != -1){
-    // TODO: ADD NOTE TO DYNAMIC STORAGE
-  }
+  // TODO: ADD NOTE TO DYNAMIC STORAGE
+  Serial.print("note detected: ");
+  Serial.print(freq.read());
+  Serial.print("; probability: ");
+  Serial.println(freq.probability());
+  Serial.print("MIDI note #");
+  Serial.println(note.mNote);
 }
